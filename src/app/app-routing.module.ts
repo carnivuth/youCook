e3071a3cd78c62a376b1b5e8fdb21c0677181035
carnivuth <matti200042@gmail.com>
2022-11-24 20:17:10 +0100

    added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RecipesListComponent } from './recipes-list/recipes-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component:LoginComponent },
+  {path: 'recipes', component: RecipesListComponent },
+  {path: '',   redirectTo: '/login', pathMatch: 'full'},
+ 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
