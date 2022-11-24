7e5dd24e0c5f861d2db16ad724a39db1a429447e
carnivuth <matti200042@gmail.com>
2022-11-24 20:17:44 +0100

    added database mock for testing

--- src/app/dbmock.ts
+++ src/app/dbmock.ts
@@ -7,35 +7,59 @@ export const users: User[] = [
     email: 'example@gmail.com',
     recipies: [
       {
-        name: 'pollo al kurry',
+        name: 'pollo al curry',
+        description: ' pollo cotto in padella con il curry',
         ingredients: [
-            {
-                quantity:10,
-                ingridient:{
-                    name:"pollo"
-
-                }
+          {
+            quantity: 10,
+            ingridient: {
+              name: 'pollo',
             },
-            {
-                quantity:5,
-                ingridient:{
-                    name:"latte"
-
-                }
+          },
+          {
+            quantity: 5,
+            ingridient: {
+              name: 'latte',
             },
-            {
-                quantity:6,
-                ingridient:{
-                    name:"kurry"
-
-                }
-            }
+          },
+          {
+            quantity: 6,
+            ingridient: {
+              name: 'curry',
+            },
+          },
+        ],
+        steps: [
+          {
+            name: 'tagliare il pollo',
+            description: 'tagliare il pollo a cubetti',
+          },
+          {
+            name: 'infarinare il pollo',
+            description: 'passare i cubetti di pollo nella farina',
+          },
+          {
+            name: 'rosolare',
+            description: 'rosolare il pollo fino a che non prende colore',
+          },
+          {
+            name: 'aggiungere latte',
+            description: 'aggiungere mezza tazza di latte',
+          },
+          {
+            name: 'aggiunggere curry',
+            description: 'aggiungere curry per insaporire',
+          },
+          {
+            name: 'cottura',
+            description: 'lasciar cuocere per 20 minuti',
+          },
         ],
       },
     ],
-    family:{
-        name:"famiglia",
-        description:"gruppo famiglia"
-    }
+    family: {
+      name: 'famiglia',
+      description: 'gruppo famiglia',
+    },
   },
 ];
