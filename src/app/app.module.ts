9ac190454fa3e7d10cc175ca0f3cef5a3285b2f1
carnivuth <matti200042@gmail.com>
2022-11-24 20:17:28 +0100

    updated app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RecipesListComponent } from './recipes-list/recipes-list.component';
+import { RecipeComponent } from './recipe/recipe.component';
+import { NewRecipeComponent } from './new-recipe/new-recipe.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RecipesListComponent,
+    RecipeComponent,
+    NewRecipeComponent,
+    HeaderComponent,
+    FooterComponent,
+
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     AppRoutingModule
   ],
