060640c4aa945cec17f10d9de835c29ec385a5da
carnivuth <matti200042@gmail.com>
2023-08-10 09:54:06 +0200

    fixed paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
 import { RecipesListComponent } from './recipes-list/recipes-list.component';
 
 const routes: Routes = [
-  {path: 'login', component:LoginComponent },
   {path: 'recipes', component: RecipesListComponent },
-  {path: '',   redirectTo: '/login', pathMatch: 'full'},
+  {path: '',   redirectTo: '/recipes', pathMatch: 'full'},
  
 ];
 
