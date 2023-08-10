2eaa8f3681c5509a1e84c1cc91963c912a7a503f
carnivuth <matti200042@gmail.com>
2023-08-10 09:54:38 +0200

    fixed removed components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,29 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
 import { RecipesListComponent } from './recipes-list/recipes-list.component';
 import { RecipeComponent } from './recipe/recipe.component';
 import { NewRecipeComponent } from './new-recipe/new-recipe.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NbThemeModule, NbLayoutModule, NbCardModule } from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
     RecipesListComponent,
     RecipeComponent,
     NewRecipeComponent,
-    HeaderComponent,
-    FooterComponent,
 
   ],
   imports: [
     FormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot({ name: 'dark' }),
+    NbLayoutModule,
+    NbCardModule,
+    NbEvaIconsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
