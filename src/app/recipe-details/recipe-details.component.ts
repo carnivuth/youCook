01c5d6aca042e95f3556428adf26133ae05eca8f
carnivuth <matti200042@gmail.com>
2023-08-10 22:10:57 +0200

    added recipe details component

--- src/app/recipe-details/recipe-details.component.ts
+++ src/app/recipe-details/recipe-details.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { DatabaseService } from 'src/database.service';
+import { Ingridient } from '../model/Ingredient';
+import { Step } from '../model/Step';
 
 @Component({
   selector: 'app-recipe-details',
@@ -7,4 +11,18 @@ import { Component } from '@angular/core';
 })
 export class RecipeDetailsComponent {
 
+constructor(private route: ActivatedRoute , private db:DatabaseService) {}
+  public ingredients?:Ingridient[]
+  public steps?:Step[]
+  
+  ngOnInit(){
+    var id = this.route.snapshot.paramMap.get('id');
+    this.db.getRecipesIngredients(id as string).subscribe(res=>{
+      this.ingredients=res as Ingridient[];
+    })
+    this.db.getRecipesSteps(id as string).subscribe(res=>{
+      this.steps=res as Step[];
+    })
+
+  }
 }
