20c418b91facf4dbd54c117542a94406f100a179
carnivuth <matti200042@gmail.com>
2023-08-13 22:30:48 +0200

    removed dbmock

--- src/database.service.ts
+++ src/database.service.ts
@@ -1,33 +1,39 @@
-import { EventEmitter, Injectable } from '@angular/core';
-import { users } from './app/dbmock';
-import { User } from './app/model/User';
+import { Injectable } from '@angular/core';
 import { Recipe } from './app/model/Recipe';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DatabaseService {
-  private users: User[] = users;
   private recipes?: Recipe[];
-  
+
   //APIS
   private base: string = 'http://localhost:8039/';
   private urlRecipes: string = this.base + 'recipes';
+  private urlIngredients: string = this.base + 'ingredients';
 
-  constructor(private _http:HttpClient) {}
+  constructor(private _http: HttpClient) {}
   public getRecipes() {
-    return this._http.get(this.urlRecipes)  
+    return this._http.get(this.urlRecipes);
   }
-  public getRecipesIngredients( id:String) {
-    return this._http.get(this.urlRecipes+"/"+id+"/ingredients");  
-  }
-
-  public getRecipe( id:String) {
-    return this._http.get(this.urlRecipes+"/"+id);  
-  }
-  public getRecipesSteps( id:String) {
-    return this._http.get(this.urlRecipes+"/"+id+"/steps");  
+  public getRecipesIngredients(id: String) {
+    return this._http.get(this.urlRecipes + '/' + id + '/ingredients');
   }
 
+  public getRecipe(id: String) {
+    return this._http.get(this.urlRecipes + '/' + id);
+  }
+  public getRecipesSteps(id: String) {
+    return this._http.get(this.urlRecipes + '/' + id + '/steps');
+  }
+  public getIngredients(query: String) {
+    return this._http.get(this.urlIngredients + '/' + query);
+  }
+  public addRecipe(recipe: Recipe) {
+    const headers = new HttpHeaders({ 'Content-type': 'application/json' });
+    return this._http.post(this.urlRecipes + '/new', JSON.stringify(recipe), {
+      headers,
+    });
+  }
 }
