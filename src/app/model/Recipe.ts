5fdad8a56d35fce6af1f436adf75a601e3f619a0
carnivuth <matti200042@gmail.com>
2023-08-13 22:31:05 +0200

    changed model

--- src/app/model/Recipe.ts
+++ src/app/model/Recipe.ts
@@ -1,13 +1,14 @@
 
+import { Ingridient } from "./Ingredient";
 import { Step } from "./Step"
 import { Use } from "./Use";
 
 export class Recipe{
     public id?:number;
-    public name?:String;
+    public name?:String; 
     public description?:String;
     public instructions?:String;
     public steps?:Step[];
-    public ingredients?:Use[];
+    public ingredients?:Ingridient[];
 
 }
