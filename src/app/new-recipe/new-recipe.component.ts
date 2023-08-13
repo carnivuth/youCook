5aebb30e1a9d9e4ffccd1aa4477e0844056e08f8
carnivuth <matti200042@gmail.com>
2023-08-13 22:31:27 +0200

    added create recipe interaction

--- src/app/new-recipe/new-recipe.component.ts
+++ src/app/new-recipe/new-recipe.component.ts
@@ -2,40 +2,60 @@ import { Component } from '@angular/core';
 import { Recipe } from '../model/Recipe';
 import { Ingridient } from '../model/Ingredient';
 import { UntypedFormBuilder } from '@angular/forms';
+import { DatabaseService } from 'src/database.service';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-new-recipe',
   templateUrl: './new-recipe.component.html',
-  styleUrls: ['./new-recipe.component.css']
+  styleUrls: ['./new-recipe.component.css'],
 })
 export class NewRecipeComponent {
- public name?:string;
- public description?:string;
- public instructions?:string;
- public quantity?:string;
- public ingredient?:string;
- public ingredients: Ingridient[] = [];
-
- public addRecipe(){
-  if(this.name!=null && this.description!=null){
-    let r=new Recipe();
-    r.name=this.name;
-    r.description=this.description;
-
-  }
- }
-
- public suggestIngredients(){
- }
-
- public addIngredient(){
-  var  i:Ingridient = new Ingridient;
-  if(this.ingredient!=undefined && this.quantity!=undefined &&
-    this.ingredient!="" && this.quantity!="" ){
-    i.name=this.ingredient;
-    i.quantity=this.quantity;
-      this.ingredients?.push(i)
-   }
-  }
+  public name?: string;
+  public description?: string;
+  public instructions?: string;
+  public quantity?: string;
+  public ingredient?: string;
+  public ingredients: Ingridient[] = [];
+  public suggestions: Ingridient[] = [];
+  public constructor(private router: Router,private db: DatabaseService) {}
   
+  public addRecipe() {
+    if (this.name != null && this.name!="" &&
+      this.description != null && this.description!= "" &&
+      this.instructions!=null && this.instructions!= "" &&
+      this.ingredients.length!=0 ) {
+      let r = new Recipe();
+      r.name = this.name;
+      r.description = this.description;
+      r.instructions=this.instructions;
+      r.ingredients=this.ingredients;
+      this.db.addRecipe(r).subscribe(res=>{
+        this.router.navigate(["/new-recipe/"]);
+      });
+    }
+  }
+
+  public suggestIngredients() {
+    if (this.ingredient != undefined && this.ingredient != '') {
+      this.suggestions=[];
+      this.db.getIngredients(this.ingredient).subscribe(res=>{
+        this.suggestions=res as Ingridient[];
+      });
+    }
+  }
+
+  public addIngredient() {
+    var i: Ingridient = new Ingridient();
+    if (
+      this.ingredient != undefined &&
+      this.quantity != undefined &&
+      this.ingredient != '' &&
+      this.quantity != ''
+    ) {
+      i.name = this.ingredient;
+      i.quantity = this.quantity;
+      this.ingredients?.push(i);
+    }
+  }
 }
